20fd172e4120dfde27fc95d027d4dee842925ce5
herreang000 <herreang000@warren.k12.in.us>
2024-08-06 11:25:40 -0400

    Notes

--- src/Strings.java
+++ src/Strings.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Strings {
+public class Strings {
+    public static void main(String[] args) {
+            String greeting = "Hello";
+            String otherGreeting = "Software";
+            System.out.println(greeting + " " + otherGreeting + " " + "Development");
+
+            //Stings are immutable
+            String startString = "The value is: ";
+            System.out.println(startString);
+            double number = 3.14;
+            startString = startString + number;
+            System.out.println(startString);
+
+
+    }
 }
